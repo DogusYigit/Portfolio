9e5f520ebbcec1acd7d09b85f350a80d93dc1432
Doğuş Yiğit Elmalı <109156947+DogusYigit@users.noreply.github.com>
2025-10-02 03:17:18 +0200

    Js button changes

--- js/scripts.js
+++ js/scripts.js
@@ -46,12 +46,11 @@
     });
 
     // Scroll to first element
-    $('#lead-down span').click(function() {
-        var scrollDistance = $('#lead').next().offset().top;
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, 500);
-    });
+   $('#lead-down span').click(function() {
+    $('html, body').animate({
+        scrollTop: $('#about').offset().top
+    }, 500);
+});
 
     // Create timeline
     $('#experience-timeline').each(function() {
